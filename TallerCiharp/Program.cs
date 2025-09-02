7a727dc16dc1cc50bf1732919e4a88972dae4e0e
Ariane Hoyos <n00478352@upn.pe>
2025-09-02 18:11:08 -0500

    Agregar archivos de proyecto.

---
+++ TallerCiharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerCiharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir mensaje 
+            Console.WriteLine("Bienvenido a C#");
+
+            //-- Definir Variables--
+            int a, b, producto;
+
+            // Entrada Datos
+            Console.WriteLine("Ingrese Primer Numero:");
+            //Guardar valor de la consola
+            a= int.Parse(Console.ReadLine());
+            a=Convert.ToInt32(Console.ReadLine());
+            //a = int.TryParse(consale. Readline())); //Boolean True o False
+
+            Console.WriteLine("Ingrese Segundo Numero:");
+            //Guardar valor de la consola
+            b = Convert.ToInt32(Console.ReadLine());
+            //b = int.TryParse(consale. Readline())); //Boolean True o False
+
+            //--Proceso--
+            producto = a * b;
+            Console.WriteLine("El producto es: " + (producto));
+            Console.WriteLine($"El producto de  {a} *  {b}  es igual {producto}"); 
+          
+            //Detener pantalla para mostrar el resultado
+            Console.ReadKey();
+           
+        }
+    }
+}
